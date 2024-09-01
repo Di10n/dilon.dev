4897193e93b2691697fd561d6ee806119a547912
Di10n <dilonchugh@gmail.com>
2024-08-31 17:49:09 -0700

    replace with temp page

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    fontFamily: {
+      'body': ["Open Sans"]
+    }
+  },
+  plugins: [],
+};
