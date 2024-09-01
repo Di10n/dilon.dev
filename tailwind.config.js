d88990f90287fe32055f46c891796b69f752e309
Di10n <dilonchugh@gmail.com>
2024-08-31 18:34:15 -0700

    mobile css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["*.html"],
   theme: {
+    screens: {
+      '2xl': {'max': '1535px'},
+      'xl': {'max': '1279px'},
+      'lg': {'max': '1023px'},
+      'md': {'max': '767px'},
+      'sm': {'max': '639px'},
+      'WRAP': {'max': '565px'}
+    },
     fontFamily: {
       'body': ["Open Sans"]
     }
